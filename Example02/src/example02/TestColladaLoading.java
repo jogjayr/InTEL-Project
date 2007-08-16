c9051f1613410ab70421a21596f2d173202c95c4
Calvin Ashmore <cashmore@gatech.edu>
2007-08-16 19:25:19 +0000

    Collada importer completed. Works using TestColladaLoading in Example02.

--- Example02/src/example02/TestColladaLoading.java
+++ Example02/src/example02/TestColladaLoading.java
@@ -9,11 +9,12 @@ import com.jme.app.SimpleGame;
 import com.jme.input.FirstPersonHandler;
 import com.jme.input.KeyBindingManager;
 import com.jme.input.KeyInput;
-import com.jme.light.PointLight;
+import com.jme.light.Light;
+import com.jme.light.LightNode;
 import com.jme.math.Vector3f;
-import com.jme.renderer.ColorRGBA;
 import com.jme.util.BoneDebugger;
-import com.jmex.model.collada.ColladaImporter;
+//import com.jmex.model.collada.ColladaImporter;
+import edu.gatech.newcollada.ColladaImporter;
 
 /**
  * Shows how to load a COLLADA file and apply an animation to it.
@@ -46,80 +47,45 @@ public class TestColladaLoading extends SimpleGame {
         KeyBindingManager.getKeyBindingManager().set( "bones", KeyInput.KEY_SPACE );
         
         //Our model is Z up so orient the camera properly.
-        cam.setAxes(new Vector3f(-1,0,0), new Vector3f(0,0,1), new Vector3f(0,1,0));
-        cam.setLocation(new Vector3f(0,-100,20));
-        input = new FirstPersonHandler( cam, 80,
-                1 );
+        //cam.setAxes(new Vector3f(-1,0,0), new Vector3f(0,0,1), new Vector3f(0,1,0));
+        cam.setLocation(new Vector3f(0,20,20));
+        input = new FirstPersonHandler( cam, 80, 1 );
+        
+        //lightState.detachAll();
         
         //url to the location of the model's textures
         URL url = TestColladaLoading.class.getClassLoader().getResource("example02/assets/");
-        //this stream points to the model itself.
         
-        InputStream mobboss = TestColladaLoading.class.getClassLoader().getResourceAsStream("example02/assets/toast.dae");
-        
-        //tell the importer to load the mob boss
-        ColladaImporter.load(mobboss, url, "model");
-        
-        //we can then retrieve the skin from the importer as well as the skeleton
-        //SkinNode sn = ColladaImporter.getSkinNode(ColladaImporter.getSkinNodeNames().get(0));
-        //Bone skel = ColladaImporter.getSkeleton(ColladaImporter.getSkeletonNames().get(0));
-        
-        //for(String name : ColladaImporter.getGeometryNames())
-        //    rootNode.attachChild(ColladaImporter.getGeometry(name));
+        InputStream inputStream;
+        inputStream = getClass().getClassLoader().getResourceAsStream("example02/assets/pisa1_background.dae");
+        ColladaImporter.load(inputStream, url, "model");
+
+        /*Matrix3f rotation = new Matrix3f();
+        rotation.fromStartEndVectors(Vector3f.UNIT_Z.negate(), Vector3f.UNIT_Y);
+        ColladaImporter.getModel().setModelBound(new BoundingBox());
+        ColladaImporter.getModel().updateModelBound();
+        ColladaImporter.getModel().updateRenderState();
+        ColladaImporter.getModel().setLocalRotation(rotation);*/
         
         rootNode.attachChild(ColladaImporter.getModel());
-        //ColladaImporter.get
         
-        //LightNode ln = ColladaImporter.getLightNode("");
         
-        //System.out.println(ColladaImporter.getGeometryNames());
-        //System.out.println(ColladaImporter.getLightNodeNames());
-        
-        //System.out.println(sn);
-        //System.out.println(skel);
+        //ColladaImporter.cleanUp();
+        inputStream = getClass().getClassLoader().getResourceAsStream("example02/assets/pisa2_lights.dae");
+        ColladaImporter.load(inputStream, url, "lights");
+        for(String name : ColladaImporter.getLightNodeNames()) {
+            LightNode ln = ColladaImporter.getLightNode(name);
+            Light light = ln.getLight();
+            lightState.attach(light);
+        }
         
         //clean up the importer as we are about to use it again.
         ColladaImporter.cleanUp();
         
-        //load the animation file.
-        //ColladaImporter.load(animation, url, "anim");
-        //this file might contain multiple animations, (in our case it's one)
-        /*ArrayList<String> animations = ColladaImporter.getControllerNames();
-        if(animations != null) {
-	        System.out.println("Number of animations: " + animations.size());
-	        for(int i = 0; i < animations.size(); i++) {
-	            System.out.println(animations.get(i));
-	        }
-	        //Obtain the animation from the file by name
-	        BoneAnimation anim1 = ColladaImporter.getAnimationController(animations.get(0));
-	        
-	        //set up a new animation controller with our BoneAnimation
-	        ac = new AnimationController();
-	        ac.addAnimation(anim1);
-	        ac.setRepeatType(Controller.RT_WRAP);
-	        ac.setActive(true);
-	        ac.setActiveAnimation(anim1);
-	        
-	        //assign the animation controller to our skeleton
-	        skel.addController(ac);
-        }*/
-        
-        //attach the skeleton and the skin to the rootnode. Skeletons could possibly
-        //be used to update multiple skins, so they are seperate objects.
-        //rootNode.attachChild(sn);
-        //rootNode.attachChild(skel);
         
         rootNode.updateGeometricState(0, true);
         //all done clean up.
         ColladaImporter.cleanUp();
         
-        lightState.detachAll();
-        
-        PointLight pl = new PointLight();
-        pl.setAmbient(new ColorRGBA(0.5f,0.5f,0.5f,1));
-        pl.setDiffuse(new ColorRGBA(1,1,1,1));
-        pl.setLocation(new Vector3f(10,-50,20));
-        pl.setEnabled(true);
-        lightState.attach(pl);
     }
 }
