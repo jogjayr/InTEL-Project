ae1a3354dbe057c09fab63089e71b60dfb7bf1dc
Calvin Ashmore <cashmore@gatech.edu>
2008-05-14 03:00:57 +0000

    Contains an in progress update of the improved unit system. This breaks code and will be fixed shortly.

--- Statics/src/edu/gatech/statics/math/Unit.java
+++ Statics/src/edu/gatech/statics/math/Unit.java
@@ -2,28 +2,48 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.gatech.statics.math;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Calvin Ashmore
  */
 public enum Unit {
+
     distance,
     angle,
     force,
     moment,
     none;
-    
-    private static UnitUtils unitUtils;
-    public static void setUtils(UnitUtils utils) {unitUtils = utils;}
-    
-    public String getSuffix() {
-        return unitUtils.getSuffix(this);
+    private static Map<Unit, String> suffixMap = new HashMap<Unit, String>();
+    private static Map<Unit, Integer> precisionMap = new HashMap<Unit, Integer>();
+    private static Map<Unit, BigDecimal> displayScaleMap = new HashMap<Unit, BigDecimal>();
+
+    public static void setSuffix(Unit unit, String suffix) {
+        suffixMap.put(unit, suffix);
     }
-    
+
+    public static void setPrecision(Unit unit, int precision) {
+        precisionMap.put(unit, precision);
+    }
+
+    public static void setDisplayScale(Unit unit, BigDecimal scale) {
+        displayScaleMap.put(unit, scale);
+    }
+
+    public String getSuffix() {
+        return suffixMap.get(this);
+    }
+
     public int getDecimalPrecision() {
-        return unitUtils.getDecimalPrecision(this);
+        return precisionMap.get(this);
+    }
+
+    public BigDecimal getDisplayScale() {
+        return displayScaleMap.get(this);
     }
 }
