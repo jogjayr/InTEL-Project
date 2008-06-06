1e9c7b99a6968afb5a4afe43e66d2ab7398e0986
Jimmy Truesdell <jtruesdell@gatech.edu>
2008-06-06 01:26:16 +0000

    Keyboard is a bit more complete. Seems to be issues all over involving adding forces and stuff. Something wrong with the selection of bodies.

--- exercises/Keyboard/src/keyboard/KeyboardExercise.java
+++ exercises/Keyboard/src/keyboard/KeyboardExercise.java
@@ -13,8 +13,10 @@ import edu.gatech.statics.application.StaticsApplication;
 import edu.gatech.statics.exercise.FBDExercise;
 import edu.gatech.statics.exercise.Schematic;
 import edu.gatech.statics.math.Unit;
+import edu.gatech.statics.math.Vector3bd;
 import edu.gatech.statics.objects.Body;
 import edu.gatech.statics.objects.DistanceMeasurement;
+import edu.gatech.statics.objects.Force;
 import edu.gatech.statics.objects.Point;
 import edu.gatech.statics.objects.bodies.Beam;
 import edu.gatech.statics.objects.bodies.Cable;
@@ -45,13 +47,12 @@ public class KeyboardExercise extends FBDExercise {
         
         Unit.setSuffix(Unit.distance, " m");
         Unit.setSuffix(Unit.moment, " N*m");
-        Unit.setDisplayScale(Unit.distance, new BigDecimal(".1"));       
+        Unit.setDisplayScale(Unit.distance, new BigDecimal("5"));       
     }
     
     Point A, B, C, D, E, P, Q;
-    Pin2d jointA, jointC, jointD;
+    Pin2d jointC, jointP, jointQ;
     Roller2d jointB, jointE;
-    Fix2d jointP, jointQ;
     
     @Override
     public float getDrawScale() {
@@ -65,41 +66,63 @@ public class KeyboardExercise extends FBDExercise {
         DisplaySystem.getDisplaySystem().getRenderer().setBackgroundColor(new ColorRGBA(.2f, .2f, .9f, 1.0f));
         StaticsApplication.getApp().getCamera().setLocation(new Vector3f(0.0f, 0.0f, 65.0f));
         
-        A = new Point("-3","0","0");
-        B = new Point("3","4.3","0");
+        A = new Point("-2.9","4.3","0");
+        D = new Point("2.9","4.3","0");
+        B = new Point("2.9","0","0");
+        E = new Point("-2.9","0","0");
+//right distances but wrong positions
+//        A = new Point("-2.3","4.3","0");
+//        D = new Point("2.3","4.3","0");
+//        B = new Point("2.3","1.5","0");
+//        E = new Point("-2.3","1.5","0");       
         C = new Point("0","2.15","0");
-        D = new Point("-3","4.3","0");
-        E = new Point("3","0","0");
-        P = new Point("1","2.9","0");
-        Q = new Point("-1","2.9","0");
+        P = new Point("-1","2.9","0");
+        Q = new Point("1","2.9","0");
         
-        Body strut1 = new Beam(B,A);
-        Body strut2 = new Beam(D,B);
-        Body strut3 = new Beam(P,Q);
-        Body strut4 = new Beam(E,D);
+        Body leftLeg = new Beam(B,A);
+        Body bar = new Beam(P,Q);
+        Body rightLeg = new Beam(E,D);
         
-        strut1.setName("Strut 1");
-        strut2.setName("Strut 2");
-        strut3.setName("Strut 3");
-        strut4.setName("Strut 4");
+        leftLeg.setName("Left Leg");
+        bar.setName("Bar");
+        rightLeg.setName("Right Leg");
         
         jointC = new Pin2d(C);
-        jointD = new Pin2d(D);
+        jointP = new Pin2d(P);
+        jointQ = new Pin2d(Q);
         
         jointB = new Roller2d(B);
-                
-        jointP = new Fix2d(P);
-        jointQ = new Fix2d(Q);
+        jointE = new Roller2d(E);
         
-        DistanceMeasurement distance2 = new DistanceMeasurement(B, D);
+        DistanceMeasurement distance1 = new DistanceMeasurement(B, D);
+        distance1.createDefaultSchematicRepresentation(.5f);
+        schematic.add(distance1);
+
+        DistanceMeasurement distance2 = new DistanceMeasurement(A, D);
         distance2.createDefaultSchematicRepresentation(.5f);
-        schematic.add(distance2);
+        schematic.add(distance2);        
         
-        jointB.attach(strut1, strut2);
-        jointC.attach(strut1, strut4);
-        jointD.attach(strut2, strut3);
-        jointP.attach(strut1, strut3);
-        jointQ.attach(strut3, strut4);
+        DistanceMeasurement distance3 = new DistanceMeasurement(Q, D);
+        distance3.createDefaultSchematicRepresentation(.5f);
+        schematic.add(distance3);
+        
+        DistanceMeasurement distance4 = new DistanceMeasurement(C, Q);
+        distance4.createDefaultSchematicRepresentation(.5f);
+        schematic.add(distance4);
+        
+        Force keyboardLeft = new Force(A, Vector3bd.UNIT_Y.negate(), new BigDecimal(50));
+        keyboardLeft.setName("Keyboard Left");
+        leftLeg.addObject(keyboardLeft);
+
+        Force keyboardRight = new Force(D, Vector3bd.UNIT_Y.negate(), new BigDecimal(50));
+        keyboardRight.setName("Keyboard Right");
+        rightLeg.addObject(keyboardRight);
+        
+        jointC.attach(leftLeg, rightLeg);
+        jointP.attach(leftLeg, bar);
+        jointQ.attach(bar, rightLeg);
+        jointE.attachToWorld(rightLeg);
+        jointB.attachToWorld(leftLeg);
         
         A.setName("A");
         B.setName("B");
@@ -117,41 +140,42 @@ public class KeyboardExercise extends FBDExercise {
         P.createDefaultSchematicRepresentation();
         Q.createDefaultSchematicRepresentation();
         
-        strut1.createDefaultSchematicRepresentation();
-        strut1.setCenterOfMassPoint(C);
-        strut2.createDefaultSchematicRepresentation();
-        strut3.createDefaultSchematicRepresentation();
-        strut4.createDefaultSchematicRepresentation();
+        keyboardLeft.createDefaultSchematicRepresentation();
+        keyboardRight.createDefaultSchematicRepresentation();
+        leftLeg.createDefaultSchematicRepresentation();
+        leftLeg.getWeight().setDiagramValue(new BigDecimal(9));
+        leftLeg.setCenterOfMassPoint(C);
+        bar.createDefaultSchematicRepresentation();
+        rightLeg.createDefaultSchematicRepresentation();
         
-        schematic.add(strut1);
-        schematic.add(strut2);
-        schematic.add(strut3);
-        schematic.add(strut4);
+        schematic.add(leftLeg);
+        schematic.add(bar);
+        schematic.add(rightLeg);
         
         // THIS DOES NOT ACTUALLY SEEM TO DO ANYTHING
         float scale = 0.1f;
         
-        Representation rep = new ModelRepresentation(strut1, "keyboard/assets/", "keyboard/assets/strut1.dae");
+        Representation rep = new ModelRepresentation(leftLeg, "keyboard/assets/", "keyboard/assets/strut1.dae");
         rep.setLocalScale(scale);
-        strut1.addRepresentation(rep);
+        leftLeg.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
         
-        rep = new ModelRepresentation(strut4, "keyboard/assets/", "keyboard/assets/strut2.dae");
+        rep = new ModelRepresentation(rightLeg, "keyboard/assets/", "keyboard/assets/strut2.dae");
         rep.setLocalScale(scale);
-        strut1.addRepresentation(rep);
+        leftLeg.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
         
-        rep = new ModelRepresentation(strut3, "keyboard/assets/", "keyboard/assets/strut3.dae");
+        rep = new ModelRepresentation(bar, "keyboard/assets/", "keyboard/assets/strut3.dae");
         rep.setLocalScale(scale);
-        strut1.addRepresentation(rep);
+        leftLeg.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
         
-        rep = new ModelRepresentation(strut4, "keyboard/assets/", "keyboard/assets/background.dae");
+        rep = new ModelRepresentation(rightLeg, "keyboard/assets/", "keyboard/assets/background.dae");
         rep.setLocalScale(scale);
-        strut1.addRepresentation(rep);
+        leftLeg.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
     }
