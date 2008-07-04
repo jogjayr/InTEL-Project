22b9a20850fcde42ccf67f22ae5d856a65bf5337
Calvin Ashmore <cashmore@gatech.edu>
2008-07-04 18:42:48 +0000

    Revises the toString formatting in BodySubset.

--- Statics/src/edu/gatech/statics/exercise/BodySubset.java
+++ Statics/src/edu/gatech/statics/exercise/BodySubset.java
@@ -5,10 +5,13 @@
 package edu.gatech.statics.exercise;
 
 import edu.gatech.statics.objects.Body;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -59,5 +62,47 @@ public class BodySubset implements DiagramKey {
     @Override
     public String toString() {
         return bodies.toString();
+    }
+
+    /**
+     * This method returns a nicely formatted version of the bodies within the subset.
+     * It will return a listing that is more legible in plain English. The format
+     * depends on the number of bodies in the subset.
+     * For one body, it will return the body name,
+     * For two bodies, it will return "A and B" where A and B are the names of the bodies.
+     * For three or more, it will return something like, "A, B, C, and D".
+     * @return
+     */
+    public String toStringPretty() {
+
+        String bodyString = "";
+
+        if (bodies.size() == 0) {
+            throw new AssertionError();
+        } else if (bodies.size() == 1) {
+            // get the first body in the set
+            bodyString = bodies.iterator().next().getName();
+        } else if (bodies.size() == 2) {
+            // two bodies, no comma, joined by "and"
+            Iterator<Body> bodyIterator = bodies.iterator();
+            bodyString = bodyIterator.next().getName();
+            bodyString += " and ";
+            bodyString += bodyIterator.next().getName();
+        } else {
+            // a bunch of bodies, "A, B, and C"
+            List<Body> bodyList = new ArrayList(bodies);
+
+            bodyString = "";
+            for (int i = 0; i < bodyList.size(); i++) {
+                bodyString += bodyList.get(i).getName();
+                if (i < bodyList.size() - 1) {
+                    bodyString += ", ";
+                }
+                if (i == bodyList.size() - 2) {
+                    bodyString += "and ";
+                }
+            }
+        }
+        return bodyString;
     }
 }
--- Statics/src/edu/gatech/statics/tasks/CompleteFBDTask.java
+++ Statics/src/edu/gatech/statics/tasks/CompleteFBDTask.java
@@ -7,10 +7,6 @@ package edu.gatech.statics.tasks;
 import edu.gatech.statics.exercise.BodySubset;
 import edu.gatech.statics.exercise.Exercise;
 import edu.gatech.statics.modes.fbd.FreeBodyDiagram;
-import edu.gatech.statics.objects.Body;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  *
@@ -30,35 +26,8 @@ public class CompleteFBDTask implements Task {
     }
 
     public String getDescription() {
-        String bodyString = "???";
+//        String bodyString = "???";
 
-        if (bodies.getBodies().size() == 0) {
-            throw new AssertionError("Should not have zero bodies in a body subset");
-        } else if (bodies.getBodies().size() == 1) {
-            // get the first body in the set
-            bodyString = bodies.getBodies().iterator().next().getName();
-        } else if (bodies.getBodies().size() == 2) {
-            // two bodies, no comma, joined by "and"
-            Iterator<Body> bodyIterator = bodies.getBodies().iterator();
-            bodyString = bodyIterator.next().getName();
-            bodyString += " and ";
-            bodyString += bodyIterator.next().getName();
-        } else {
-            // a bunch of bodies, "A, B, and C"
-            List<Body> bodyList = new ArrayList(bodies.getBodies());
-
-            bodyString = "";
-            for (int i = 0; i < bodyList.size(); i++) {
-                bodyString += bodyList.get(i).getName();
-                if (i < bodyList.size() - 1) {
-                    bodyString += ", ";
-                }
-                if (i == bodyList.size() - 2) {
-                    bodyString += "and ";
-                }
-            }
-        }
-
-        return "Build a FBD out of " + bodyString;
+        return "Build a FBD out of " + bodies.toStringPretty();
     }
 }
