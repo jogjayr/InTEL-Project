b2c9aaeb4216a01aba411754612f528b3e5aae4f
Calvin Ashmore <cashmore@gatech.edu>
2008-09-03 13:40:52 +0000

    Update affects the DraggablePopupWindow, and makes it so that the user cannot drag if they are trying to press a button.

--- Statics/src/edu/gatech/statics/ui/components/DraggablePopupWindow.java
+++ Statics/src/edu/gatech/statics/ui/components/DraggablePopupWindow.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.gatech.statics.ui.components;
 
+import com.jme.input.MouseInput;
 import com.jme.system.DisplaySystem;
 import com.jmex.bui.BComponent;
 import com.jmex.bui.BPopupWindow;
@@ -19,61 +19,73 @@ import com.jmex.bui.layout.BLayoutManager;
  * @author Calvin Ashmore
  */
 public class DraggablePopupWindow extends BPopupWindow {
-    MouseListener dragControl;
-    
+
+    private MouseListener dragControl;
     private static final int WINDOW_TOLERANCE = 10;
-    
+
     public DraggablePopupWindow(BWindow parentWindow, BLayoutManager layout) {
         super(parentWindow, layout);
         dragControl = new DragController();
         addListener(dragControl);
-        
+
         setStyleClass("application_popup");
     }
-    
+
     protected void addDragHandle(BComponent component) {
         component.addListener(dragControl);
     }
-    
+
     private class DragController implements MouseListener, MouseMotionListener {
 
-        //private boolean hasMouse;
+        private boolean dragging;
         private int mouseX;
         private int mouseY;
-        
+
         public void mousePressed(MouseEvent event) {
-            //hasMouse = true;
+            dragging = true;
             mouseX = event.getX();
             mouseY = event.getY();
         }
 
         public void mouseReleased(MouseEvent event) {
-            //hasMouse = false;
+            dragging = false;
         }
 
-        public void mouseEntered(MouseEvent event) {}
-        public void mouseExited(MouseEvent event) {}
+        public void mouseEntered(MouseEvent event) {
+        }
+
+        public void mouseExited(MouseEvent event) {
+        }
 
         public void mouseMoved(MouseEvent event) {
+            if(!MouseInput.get().isButtonDown(0))
+                dragging = false;
         }
 
         public void mouseDragged(MouseEvent event) {
+            
+            if(!dragging)
+                return;
+            
             int dx = event.getX() - mouseX;
             int dy = event.getY() - mouseY;
-            
+
             // check the boundary so that we can't accidentally move the window off the screen.
-            int newX = dx+getX();
-            int newY = dy+getY();
-            
-            if(newX < WINDOW_TOLERANCE)
+            int newX = dx + getX();
+            int newY = dy + getY();
+
+            if (newX < WINDOW_TOLERANCE) {
                 newX = WINDOW_TOLERANCE;
-            if(newY < WINDOW_TOLERANCE)
+            }
+            if (newY < WINDOW_TOLERANCE) {
                 newY = WINDOW_TOLERANCE;
-            if(newX > DisplaySystem.getDisplaySystem().getWidth() - getWidth() - WINDOW_TOLERANCE)
+            }
+            if (newX > DisplaySystem.getDisplaySystem().getWidth() - getWidth() - WINDOW_TOLERANCE) {
                 newX = DisplaySystem.getDisplaySystem().getWidth() - getWidth() - WINDOW_TOLERANCE;
-            if(newY > DisplaySystem.getDisplaySystem().getHeight() - getHeight() - WINDOW_TOLERANCE)
+            }
+            if (newY > DisplaySystem.getDisplaySystem().getHeight() - getHeight() - WINDOW_TOLERANCE) {
                 newY = DisplaySystem.getDisplaySystem().getHeight() - getHeight() - WINDOW_TOLERANCE;
-            
+            }
             setLocation(newX, newY);
             mouseX = event.getX();
             mouseY = event.getY();
