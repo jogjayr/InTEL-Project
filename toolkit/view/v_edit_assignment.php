39a23518f0e0ca5b9039712ca10462981d8f994d
Daniel Upton <dupton3@gatech.edu>
2008-09-10 18:39:02 +0000

    Changes for manage/edit assignment.

---
+++ toolkit/view/v_edit_assignment.php
@@ -0,0 +1,111 @@
+<?php
+ 	require_once('admin/initvars.php');
+	requireLogin(); 
+  
+  //ensure that user is admin or instructor
+  if (!isInstructor() && !isAdmin()) {
+		redirectRel('index.php');
+	}
+  
+	$success = false;
+	$err = '';
+  
+  //if get data set the assignment id
+  if (isset($_GET['id'])){
+    $assignmentId = $_GET['id'];
+    //check if user is admin or owner of assignment
+    if(isInstructor() && isAssignmentOwner($_SESSION['uuid'], $assignmentId) || isAdmin()){
+      $assignment = getAssignmentById($assignmentId);
+    }
+    else{
+      //user shouldn't be here
+      redirectRel('index.php');
+    }
+  }
+  
+  $problem = '';
+  $description = '';
+  $openDate = '';
+  $closeDate = '';
+  
+  if ($assignment){
+    //print_r(array_keys($assignment));
+    //$assignmentId = $assignment['id'];
+    $classId = $assignment['class_id'];
+    $problemId = $assignment['problem_id'];
+    $openDate = date("m.d.y", $assignment['open_date']);
+    $closeDate = date("m.d.y",$assignment['close_date']);
+  }else{
+    echo "Assignment not found."; //FIX
+  }
+  
+  //if post data is sent update the database
+	if (isset($_POST['submit'])) {
+    $assignmentId = $_POST['assignment_id'];
+		$problemId = $_POST['problem_id'];
+    $classId = $_POST['class_id'];
+    $openDate = strtotime($_POST['open_date']);
+    $closeDate = strtotime($_POST['close_date']);
+    //ensure that the date is formatted correctly
+    if($openDate && $closeDate){
+  		if (updateAssignment($assignmentId, $problemId, $classId, $openDate, $closeDate)) {
+  			$success = true;
+  		}
+    }else{
+      $err = 'Please enter a date in the format mm/dd/yyyy.';
+    }
+	}
+  
+  //get all problems for form
+  $problems = getAllProblems();
+  //get all classes that belong to this user for form
+  $classes = getClassByOwner($_SESSION['uuid']);
+  
+	$title = 'Edit Assignment';
+	require_once('header.php');
+  
+  if ($success) {
+		para('The assignment has been updated.');
+	} else {
+		paraErr($err);
+	}
+	
+?>
+<form method="post" action="">
+  <input type="hidden" name="assignment_id" value="<?php echo $assignmentId; ?>" />
+  <p>Problem:
+    <select name="problem_id">
+      <?php
+      foreach ($problems as $prob){
+        if ($prob['id']==$problemId){
+          echo '<option value="'.$prob['id'].'" selected="selected">'.$prob['name'].'</option>';
+        }else{
+          echo '<option value="'.$prob['id'].'">'.$prob['name'].'</option>';
+        }
+      }
+      ?>
+    </select>
+  </p>
+  <p>Class: 
+    <select name="class_id">
+      <?php
+      foreach ($classes as $cls){
+        if ($cls['id']==$classId){
+          echo '<option value="'.$cls['id'].'" selected="selected">'.$cls['description'].'</option>';
+        }else{
+          echo '<option value="'.$cls['id'].'">'.$cls['description'].'</option>';
+        }
+      }
+      ?>
+    </select>
+  </p>
+  <p>Open Date (mm/dd/yyyy): <input type="text" name="open_date" value="<?php echo $openDate; ?>" /></p>
+  <p>Close Date (mm/dd/yyyy): <input type="text" name="close_date" value="<?php echo $closeDate; ?>" /></p>
+
+	<p><input type="submit" name="submit" value="Update" /></p>
+</form>
+
+<?php
+	
+	require_once('footer.php') 
+?>
