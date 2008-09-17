65f344d102110d4ed5b341c42785d71c516a1037
Calvin Ashmore <cashmore@gatech.edu>
2008-09-17 14:43:49 +0000

    fixes a straggling debugging message

--- toolkit/auto_postAssignment.php
+++ toolkit/auto_postAssignment.php
@@ -21,9 +21,6 @@ $stateData =      addslashes($_POST["state_data"]);
 $verifierKey =    addslashes($_POST["verifier_key"]);
 $timestamp =      addslashes($_POST["timestamp"]);
 
-print_r($_POST);
-echo "\n";
-
 $success = post_exercise($assignmentId, $userId, $exerciseStatus, $stateData, $verifierKey, $timestamp);
 
 // report back
