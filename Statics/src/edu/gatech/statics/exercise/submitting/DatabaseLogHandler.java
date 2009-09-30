227742fd84a3d4f2b97dee894a4e1c9fbd4a4a2e
Calvin Ashmore <cashmore@gatech.edu>
2009-09-30 18:27:22 +0000

    Commits a change to how the logger interacts with the toolkit website.

--- Statics/src/edu/gatech/statics/exercise/submitting/DatabaseLogHandler.java
+++ Statics/src/edu/gatech/statics/exercise/submitting/DatabaseLogHandler.java
@@ -4,6 +4,7 @@
  */
 package edu.gatech.statics.exercise.submitting;
 
+import edu.gatech.statics.application.StaticsApplet;
 import edu.gatech.statics.exercise.Exercise;
 import java.util.Map;
 import java.util.logging.Handler;
@@ -22,6 +23,7 @@ public class DatabaseLogHandler extends Handler {
     public void publish(LogRecord record) {
         Map<String, String> postMap = poster.getNewPostMap();
         postMap.put("problem_id", "" + Exercise.getExercise().getProblemID());
+        postMap.put("user_id", "" + Exercise.getExercise().getState().getUserID());
         postMap.put("session_id", "" + Exercise.getExercise().getSessionID());
         postMap.put("java_class", record.getSourceClassName());
         postMap.put("java_method", record.getSourceMethodName());
