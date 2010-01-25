a506a96ba27a6561ac3280513aef2fafe45f47fd
Calvin Ashmore <cashmore@gatech.edu>
2010-01-25 19:18:58 +0000

    Creates a template Spiderwoman exercise.

---
+++ exercises/Spiderwoman/src/spiderwoman/SpiderwomanExercise.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spiderwoman;
+
+import edu.gatech.statics.modes.description.Description;
+import edu.gatech.statics.modes.frame.FrameExercise;
+
+/**
+ *
+ * @author Calvin Ashmore
+ */
+public class SpiderwomanExercise extends FrameExercise {
+
+    @Override
+    public Description getDescription() {
+
+        Description description = new Description();
+
+        description.setTitle("Georgia Tech Spiderwoman");
+        description.setNarrative("...");
+
+        return description;
+    }
+
+    @Override
+    public void initExercise() {
+
+    }
+
+    @Override
+    public void loadExercise() {
+
+
+
+    }
+}
