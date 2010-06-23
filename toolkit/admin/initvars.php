f7954e0457176b10ae4f7bbf8c3567659431fc58
Calvin Ashmore <cashmore@gatech.edu>
2010-06-23 19:47:20 +0000

    more php updates.

--- toolkit/admin/initvars.php
+++ toolkit/admin/initvars.php
@@ -6,29 +6,17 @@ $maxUploadFileSizeInMegabytes = '50';
 
 ini_set('post_max_size', $maxUploadFileSizeInMegabytes . 'M');
 
-require_once('model/baseinclude.php');
-
-$site_title = "Interactive Toolkit for Engineering Learning";
-$site_email_address = "eschechter3@gatech.edu";
-
-$base_address = 'http://intel.gatech.edu/toolkit/';
-$base_file_path = '/www/virtual/intel/toolkit/';
+require_once('model/mysql.php');
 
 $dbusername = 'idtweb';
 $dbpassword = 'gt00www';
 $dbname = 'intel';
 $dbserver = 'localhost';
 
-$smtp_server = "";
-$smtp_username = "";
-$smtp_password = "";
-$smtp_default_from = $site_email_address;
-
-$ftp_server = '';
-$ftp_user_name = '';
-$ftp_user_pass = '';
-
 $db = connect($dbserver, $dbusername, $dbpassword, $dbname);
 
 session_start();
+
+require_once('model/baseinclude.php');
+
 ?>
--- toolkit/model/baseinclude.php
+++ toolkit/model/baseinclude.php
@@ -1,10 +1,17 @@
 <?php
 
-require_once('model/mysql.php');
 require_once('model/lib_data.php');
 require_once('model/m_lib_accounts.php');
 require_once('controller/c_lib_accounts.php');
 require_once('controller/util.php');
+
+
+$site_title = "Interactive Toolkit for Engineering Learning";
+$site_email_address = "eschechter3@gatech.edu";
+
+$base_address = 'http://intel.gatech.edu/toolkit/';
+$base_file_path = '/www/virtual/intel/toolkit/';
+
 
 // setup user variables.
 
