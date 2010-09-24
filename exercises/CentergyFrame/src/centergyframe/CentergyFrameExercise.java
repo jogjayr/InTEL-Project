3a5d9aa5c8428ece658c264332e663fcac54db56
Vignesh Swaminathan <vswaminathan3@gatech.edu>
2010-09-24 19:01:59 +0000

    updated centergy frame without attached model objects

---
+++ exercises/CentergyFrame/src/centergyframe/CentergyFrameExercise.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package centergyframe;
+
+import edu.gatech.statics.exercise.OrdinaryExercise;
+import edu.gatech.statics.exercise.Schematic;
+import edu.gatech.statics.modes.description.Description;
+import edu.gatech.statics.modes.description.layouts.ScrollbarLayout;
+import edu.gatech.statics.objects.Point;
+import edu.gatech.statics.objects.bodies.Bar;
+import edu.gatech.statics.objects.bodies.Beam;
+import edu.gatech.statics.objects.representations.ModelNode;
+import edu.gatech.statics.objects.representations.ModelRepresentation;
+/**
+ *
+ * @author Vignesh
+*/
+public class CentergyFrameExercise extends OrdinaryExercise {
+
+    @Override
+    public Description getDescription() {
+
+        Description description = new Description();
+
+        description.setNarrative("As you may have heard, there was a partial collapse of the parking deck behind the Centergy Building" +
+                                 "in Technology Square on Monday, June 29, 2009, around noon time. " +
+                                 "Though not on Georgia Tech campus, some Tech employees do lease parking spaces within this deck. " +
+                                 "Thankfully, nobody was injured.");
+
+        description.setProblemStatement("The first picture shows two different supports that the firefighters built to maintain " +
+                "stability of the floors adjacent to where the floors collapsed. We want to analyze the top structure in this problem. " +
+                "To simplify the analysis, let’s only look at the front plane which can be simplified as shown in the bottom picture." +
+                " We can also simplify the analysis by assuming the entire structure is supporting 30 kips, which is the approximate weight " +
+                "of the slab of concrete and cars on top of it.");
+
+        description.setGoals("Find the reaction forces at points A and F.");
+
+        description.setLayout(new ScrollbarLayout());
+
+        description.addImage("centergyframe/assets/screenshot.png");
+        description.addImage("centergyframe/assets/mainpicture.png");
+        description.addImage("centergyframe/assets/outsidebeam.png");
+        description.addImage("centergyframe/assets/insidedeck.png");
+
+        return description;
+    }
+
+
+    @Override
+    public void loadExercise() {
+        Schematic schematic = getSchematic();
+
+        Point A = new Point("A", "0", "0", "0");
+        Point B = new Point("B", "0", "5", "0");
+        Point C = new Point("C", "0", "10", "0");
+        Point D = new Point("D", "4", "10", "0");
+        Point E = new Point("E", "4", "5", "0");
+        Point F = new Point("F", "4", "0", "0");
+
+        A.createDefaultSchematicRepresentation();
+        B.createDefaultSchematicRepresentation();
+        C.createDefaultSchematicRepresentation();
+        D.createDefaultSchematicRepresentation();
+        E.createDefaultSchematicRepresentation();
+        F.createDefaultSchematicRepresentation();
+
+        schematic.add(A);
+        schematic.add(B);
+        schematic.add(C);
+        schematic.add(D);
+        schematic.add(E);
+        schematic.add(F);
+
+        Bar BE = new Bar("BE", B, E);
+        Beam AC = new Beam("AC", A, C);
+        Beam CD = new Beam("CD", C, D);
+        Beam DF = new Beam("DF", D, F);
+
+        schematic.add(BE);
+        schematic.add(AC);
+        schematic.add(CD);
+        schematic.add(DF);
+
+        //Importing Models
+        ModelNode modelNode = ModelNode.load("centergyframe/assets/", "centergyframe/assets/CenturgyB_collada.dae");
+        modelNode.extractLights();
+
+        //Specifying the root folder for all the model objects that need to be attached to the exercise elements (points)
+        ModelRepresentation rep;
+        String prefix = "VisualSceneNode/everything/everything_group1/Scene/";
+
+        //Attaching a model object to it's respective exercise element
+        rep = modelNode.extractElement(AC, prefix+"pCube21");
+        AC.addRepresentation(rep);
+        rep.setSynchronizeRotation(false);
+        rep.setSynchronizeTranslation(false);
+
+
+        rep = modelNode.getRemainder(schematic.getBackground());
+        schematic.getBackground().addRepresentation(rep);
+    }
+   
+}
+
+
+
+
--- exercises/CentergyTruss/src/centergytruss/CentergyTrussExercise.java
+++ exercises/CentergyTruss/src/centergytruss/CentergyTrussExercise.java
@@ -2,54 +2,61 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package centergytruss;
 
-import edu.gatech.statics.exercise.OrdinaryExercise;
 import edu.gatech.statics.exercise.Schematic;
+import edu.gatech.statics.math.Vector3bd;
 import edu.gatech.statics.modes.description.Description;
 import edu.gatech.statics.modes.description.layouts.ScrollbarLayout;
+import edu.gatech.statics.modes.truss.TrussExercise;
+import edu.gatech.statics.objects.Force;
 import edu.gatech.statics.objects.Point;
 import edu.gatech.statics.objects.bodies.Bar;
+import edu.gatech.statics.objects.bodies.PointBody;
+import edu.gatech.statics.objects.connectors.Connector2ForceMember2d;
+import edu.gatech.statics.objects.connectors.Pin2d;
+import edu.gatech.statics.objects.connectors.Roller2d;
 import edu.gatech.statics.objects.representations.ModelNode;
 import edu.gatech.statics.objects.representations.ModelRepresentation;
+import edu.gatech.statics.tasks.Solve2FMTask;
+import java.math.BigDecimal;
 
 /**
  *
  * @author Vignesh 
  */
-public class CentergyTrussExercise extends OrdinaryExercise {
+public class CentergyTrussExercise extends TrussExercise {
 
     @Override
     public Description getDescription() {
         Description description = new Description();
 
-        description.setNarrative("As you may have heard, there was a partial collapse of the parking deck behind the Centergy" +
-                "Building in Technology Square on Monday, June 29, 2009, around noon time. Though not on" +
-                "Georgia Tech campus, some Tech employees do lease parking spaces within this deck." +
-                "Thankfully, nobody was injured.");
+        description.setNarrative("As you may have heard, there was a partial collapse of the parking deck behind the Centergy"
+                + "Building in Technology Square on Monday, June 29, 2009, around noon time. Though not on"
+                + "Georgia Tech campus, some Tech employees do lease parking spaces within this deck."
+                + "Thankfully, nobody was injured.");
 
-        description.setProblemStatement("The first picture shows two different supports that the firefighters built to maintain stability of " +
-                "the floors adjacent to where the floors collapsed. We want to analyze the top structure in this" +
-                "problem. To simplify the analysis, let’s only look at the front plane which can be simplified as " +
-                "shown in the bottom picture. We can also simplify the analysis by assuming the entire structur" +
-                "is supporting 30 kips, which is the approximate weight of the slab of concrete and cars on top of" +
-                "it.  We can simplify the drawings slightly more by looking at strictly the bottom half which will" +
-                "help in our analysis of the truss. We can translate the forces from the top of the structure down to" +
-                "points B and C.");
+        description.setProblemStatement("The first picture shows two different supports that the firefighters built to maintain stability of "
+                + "the floors adjacent to where the floors collapsed. We want to analyze the top structure in this"
+                + "problem. To simplify the analysis, let’s only look at the front plane which can be simplified as "
+                + "shown in the bottom picture. We can also simplify the analysis by assuming the entire structur"
+                + "is supporting 30 kips, which is the approximate weight of the slab of concrete and cars on top of"
+                + "it.  We can simplify the drawings slightly more by looking at strictly the bottom half which will"
+                + "help in our analysis of the truss. We can translate the forces from the top of the structure down to"
+                + "points B and C.");
 
         description.setGoals(
-                "Solve for the forces in BA, BD, and CD using either the method of joints or the method of " +
-                "sections. Specify whether each member is in tension, compression or is a zero force member.");
+                "Solve for the forces in BA, BD, and CD using either the method of joints or the method of "
+                + "sections. Specify whether each member is in tension, compression or is a zero force member.");
 
-            description.setLayout(new ScrollbarLayout());
+        description.setLayout(new ScrollbarLayout());
 
-            description.addImage("centergytruss/assets/screenshot.png");
-            description.addImage("centergytruss/assets/mainpicture.png");
-            description.addImage("centergytruss/assets/outsidebeam.png");
-            description.addImage("centergytruss/assets/insidedeck.png");
-        
-            return description;
+        description.addImage("centergytruss/assets/screenshot.png");
+        description.addImage("centergytruss/assets/mainpicture.png");
+        description.addImage("centergytruss/assets/outsidebeam.png");
+        description.addImage("centergytruss/assets/insidedeck.png");
+
+        return description;
     }
 
     @Override
@@ -60,6 +67,12 @@ public class CentergyTrussExercise extends OrdinaryExercise {
         Point B = new Point("B", "0", "5", "0");
         Point C = new Point("C", "4", "5", "0");
         Point D = new Point("D", "4", "0", "0");
+
+        PointBody Ab, Bb, Cb, Db;
+        Ab = new PointBody("Joint A", A);
+        Bb = new PointBody("Joint B", B);
+        Cb = new PointBody("Joint C", C);
+        Db = new PointBody("Joint D", D);
 
         Bar AB = new Bar("AB", A, B);
         Bar BC = new Bar("BC", B, C);
@@ -72,11 +85,55 @@ public class CentergyTrussExercise extends OrdinaryExercise {
         C.createDefaultSchematicRepresentation();
         D.createDefaultSchematicRepresentation();
 
+        Ab.createDefaultSchematicRepresentation();
+        Bb.createDefaultSchematicRepresentation();
+        Cb.createDefaultSchematicRepresentation();
+        Db.createDefaultSchematicRepresentation();
 
-        schematic.add(A);
-        schematic.add(B);
-        schematic.add(C);
-        schematic.add(D);
+        Force f1 = new Force(B, Vector3bd.UNIT_Y.negate(), new BigDecimal("15"));
+        Force f2 = new Force(C, Vector3bd.UNIT_Y.negate(), new BigDecimal("15"));
+        f1.setName("f1");
+        f2.setName("f2");
+        f1.createDefaultSchematicRepresentation();
+        f2.createDefaultSchematicRepresentation();
+
+        Bb.addObject(f1);
+        Cb.addObject(f2);
+
+        Connector2ForceMember2d connector;
+        connector = new Connector2ForceMember2d(A, AB);
+        connector.attach(AB, Ab);
+        connector = new Connector2ForceMember2d(A, DA);
+        connector.attach(DA, Ab);
+        connector = new Connector2ForceMember2d(B, AB);
+        connector.attach(AB, Bb);
+        connector = new Connector2ForceMember2d(B, BD);
+        connector.attach(BD, Bb);
+        connector = new Connector2ForceMember2d(B, BC);
+        connector.attach(BC, Bb);
+        connector = new Connector2ForceMember2d(C, BC);
+        connector.attach(BC, Cb);
+        connector = new Connector2ForceMember2d(C, CD);
+        connector.attach(CD, Cb);
+        connector = new Connector2ForceMember2d(D, CD);
+        connector.attach(CD, Db);
+        connector = new Connector2ForceMember2d(D, BD);
+        connector.attach(BD, Db);
+        connector = new Connector2ForceMember2d(D, DA);
+        connector.attach(DA, Db);
+
+        Pin2d pinA = new Pin2d(A);
+        Roller2d rollerD = new Roller2d(D);
+        pinA.setName("pin A");
+        rollerD.setName("pin D");
+        pinA.attachToWorld(Ab);
+        rollerD.attachToWorld(Db);
+        rollerD.setDirection(Vector3bd.UNIT_Y);
+        
+        schematic.add(Ab);
+        schematic.add(Bb);
+        schematic.add(Cb);
+        schematic.add(Db);
         schematic.add(AB);
         schematic.add(BC);
         schematic.add(CD);
@@ -89,33 +146,37 @@ public class CentergyTrussExercise extends OrdinaryExercise {
         ModelRepresentation rep;
         String prefix = "RootNode/group1/Scene/completeStructure/";
 
-        rep = modelNode.extractElement(AB, prefix+"pCube3");
+        rep = modelNode.extractElement(AB, prefix + "pCube3");
         AB.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
 
-        rep = modelNode.extractElement(BC, prefix+"pCube27");
+        rep = modelNode.extractElement(BC, prefix + "pCube27");
         BC.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
 
-        rep = modelNode.extractElement(CD, prefix+"pCube2");
+        rep = modelNode.extractElement(CD, prefix + "pCube2");
         CD.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
 
-        rep = modelNode.extractElement(DA, prefix+"pCube16");
+        rep = modelNode.extractElement(DA, prefix + "pCube16");
         DA.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
 
-        rep = modelNode.extractElement(BD, prefix+"pCube20");
+        rep = modelNode.extractElement(BD, prefix + "pCube20");
         BD.addRepresentation(rep);
         rep.setSynchronizeRotation(false);
         rep.setSynchronizeTranslation(false);
 
         rep = modelNode.getRemainder(schematic.getBackground());
         schematic.getBackground().addRepresentation(rep);
-    }
 
+
+        addTask(new Solve2FMTask("Solve ABˆ", AB, AB.getConnector1()));
+        addTask(new Solve2FMTask("Solve BD", BD, BD.getConnector1()));
+        addTask(new Solve2FMTask("Solve CD", CD, CD.getConnector1()));
+    }
 }
