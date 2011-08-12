e168d5cd82a74616c1980a3d26a7230f5752e438
Jayraj Jog <jjog3@gatech.edu>
2011-08-12 02:12:28 +0000

    added GPL header to sources

--- Statics/src/edu/gatech/statics/application/StaticsApplication.java
+++ Statics/src/edu/gatech/statics/application/StaticsApplication.java
@@ -1,4 +1,21 @@
 /*
+ *  This file is part of InTEL, the Interactive Toolkit for Engineering Learning.
+ *  http://intel.gatech.edu
+ *
+ *  InTEL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  InTEL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with InTEL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * StaticsApplication.java
  *
  * Created on June 11, 2007, 2:09 PM
